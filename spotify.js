d6ce649dccd665a404644d476ca703395897616f
Andrea Trentini <andrea.trentini@marconirovereto.it>
2023-11-09 11:17:28 +0100

    Completato elenco copertine

--- spotify.js
+++ spotify.js
@@ -46,18 +46,46 @@ function getArtistsList(artistName) {
         console.log(response);
         let artisti = response.artists.items;
         let principale = document.getElementById('principale');
+        principale.innerHTML = '';
         let lista = createArtistsList(artisti);
         principale.appendChild(lista);
         console.log(artisti)
     })
 }
 function getArtistsAlbums(artistId) {
+    // https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/albums
     console.log(artistId);
+    fetch('https://api.spotify.com/v1/artists/' + artistId +'/albums', {
+        headers: {
+            Authorization: 'Bearer '+ tokenBearer
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        let albums = response.items;
+        console.log(albums);
+        let principale = document.getElementById('principale');
+        principale.innerHTML = '';
+        principale.appendChild(createAlbumsList(albums));
+    })
 }
 
 function ricerca() {
     let artistName = document.getElementById('inputName').value;
     getArtistsList(artistName);
+}
+
+function createAlbumsList(albums) {
+    let contenitore = document.createElement('div');
+    albums.forEach(album => {
+        let copertina = document.createElement('img');
+        copertina.src = album.images[1].url;
+        copertina.onclick = () => {
+            console.log(album.id);
+        };
+        contenitore.appendChild(copertina);
+    });
+    return contenitore;
 }
 
 function createArtistsList(artisti) {
